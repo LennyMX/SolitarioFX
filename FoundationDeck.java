c41e613651039a95ceb2320e34aa4c1ba92a2314
LennyMX <patron.luis@uabc.edu.mx>
2026-03-08 15:58:15 -0700

    clase foundation implementando pilas

---
+++ FoundationDeck.java
@@ -0,0 +1,107 @@
+package com.example.demoxxx.solitaire;
+
+import com.example.demoxxx.DeckOfCards.CartaInglesa;
+import com.example.demoxxx.DeckOfCards.Palo;
+import com.example.demoxxx.Pila;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+/**
+ * Modela un monículo donde se ponen las cartas
+ * de un solo palo.
+ *
+ * @author Cecilia M. Curlango
+ * @version 2025
+ */
+public class FoundationDeck {
+    Palo palo;
+    Pila<CartaInglesa> cartas;
+
+
+    public FoundationDeck(Palo palo) {
+        this.palo = palo;
+        cartas = new Pila<>(13);
+    }
+
+    public FoundationDeck(CartaInglesa carta) {
+        palo = carta.getPalo();
+        cartas = new Pila<>(13);
+
+        if (carta.getValorBajo() == 1) {
+            cartas.push(carta);
+        }
+    }
+
+    /**
+     * Agrega una carta al montículo. Sólo la agrega si
+     * la carta es del palo del montículo y el la siguiente
+     * carta en la secuencia.
+     *
+     * @param carta que se intenta almancenar
+     * @return true si se pudo guardar la carta, false si no
+     */
+    public boolean agregarCarta(CartaInglesa carta) {
+        boolean agregado = false;
+
+        if (carta.tieneElMismoPalo(palo)) {
+
+            if (cartas.pilaVacia()) {
+                if (carta.getValorBajo() == 1) {
+                    cartas.push(carta);
+                    agregado = true;
+                }
+            } else {
+                CartaInglesa ultimaCarta = cartas.peek();
+
+                if (ultimaCarta.getValorBajo() + 1 == carta.getValorBajo()) {
+                    cartas.push(carta);
+                    agregado = true;
+                }
+            }
+        }
+
+        return agregado;
+    }
+
+
+    /**
+     * Remover la última carta del montículo.
+     *
+     * @return la carta que removió, null si estaba vacio
+     */
+    CartaInglesa removerUltimaCarta() {
+        if (!cartas.pilaVacia()) {
+            return cartas.pop();
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        if (cartas.pilaVacia()) {
+            return "---";
+        }
+        return cartas.peek().toString();
+    }
+
+    /**
+     * Determina si hay cartas en el Foundation.
+     * @return true hay al menos una carta, false no hay cartas
+     */
+    public boolean estaVacio() {
+        return cartas.pilaVacia();
+    }
+
+    /**
+     * Obtiene la última carta del Foundation sin removerla.
+     * @return última carta, null si no hay cartas
+     */
+    public CartaInglesa getUltimaCarta() {
+        return cartas.peek();
+    }
+
+    public Palo getPalo() {
+        return palo;
+    }
+}
